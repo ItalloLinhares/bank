4756112df3e06088f478e38981f8f1135a73818c
ItalloLinhares <45083049+ItalloLinhares@users.noreply.github.com>
2022-01-11 11:45:29 -0300

    Adding form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewBankTransferComponent } from './new bank transfer/new-bank-transfer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewBankTransferComponent
   ],
   imports: [
     BrowserModule
