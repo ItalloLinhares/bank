c3b56b276c037897e1a5bf5fb65390750730d274
ItalloLinhares <45083049+ItalloLinhares@users.noreply.github.com>
2022-01-11 18:53:02 -0300

    adding output

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Output } from '@angular/core';
+import { EventEmitter } from 'protractor';
 
 @Component({
   selector: 'app-nova-transferencia',
@@ -6,7 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./nova-transferencia.component.scss']
 })
 export class NovaTransferenciaComponent {
+
+    @Output() aoTransferir = new EventEmitter<any>();
+
+    valor: number;
+    destino: number;
+
     transferir(){
       console.log('Solicitada a transferencia')
+      console.log('Valor: ', this.valor)
+      console.log('Destino: ', this.destino)
     }
 }
